e5d364ebee85ef89e94ca4500424daef5ceb11b2
LKKitty <1049887398@qq.com>
2020-07-01 11:51:28 +0800

    feat: 修改配置

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,7 @@
 import Vue from "vue";
 import App from "./App.vue";
 
-// import "@/styles/reset.scss";
-// import "@/styles/index.scss";
-
+import "@/styles/reset.scss";
 Vue.config.productionTip = false;
 
 new Vue({
--- vue.config.js
+++ vue.config.js
@@ -22,7 +22,50 @@ module.exports = {
       patterns: [
         path.resolve(__dirname, "src/styles/_variables.scss"),
         path.resolve(__dirname, "src/styles/_mixins.scss"),
-      ],
+      ]
     }
+  },
+  chainWebpack(config) {
+    // provide the app's title in webpack's name field, so that
+    // it can be accessed in index.html to inject the correct title.
+    config.set("name", process.env.VUE_APP_TITLE);
+
+    // https://webpack.js.org/configuration/devtool/#development
+    config.when(process.env.NODE_ENV === "development", (config) =>
+      config.devtool("cheap-eval-source-map")
+    );
+
+    // remove vue-cli-service's progress output
+    config.plugins.delete("progress");
+    // replace with another progress output plugin to solve the this bug:
+    // https://github.com/vuejs/vue-cli/issues/4557
+    config
+      .plugin("simple-progress-webpack-plugin")
+      .use(require.resolve("simple-progress-webpack-plugin"), [
+        {
+          format: "compact"
+        }
+      ]);
+    config.when(process.env.NODE_ENV !== "development", (config) => {
+      config.optimization.splitChunks({
+        chunks: "all",
+        cacheGroups: {
+          libs: {
+            name: "chunk-libs",
+            test: /[\\/]node_modules[\\/]/,
+            priority: 10,
+            chunks: "initial", // only package third parties that are initially dependent
+          },
+          commons: {
+            name: "chunk-commons",
+            test: path.resolve(__dirname, "src/components"),
+            minChunks: 3, //  minimum common number
+            priority: 5,
+            reuseExistingChunk: true,
+          },
+        },
+      });
+      config.optimization.runtimeChunk("single");
+    });
   }
 };
