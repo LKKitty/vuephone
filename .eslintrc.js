a615c05f093ff1d7b70e7322bd0a38018e50ce63
LKKitty <1049887398@qq.com>
2020-07-01 09:48:50 +0800

    feat:添加eslint规则

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,17 +4,19 @@ module.exports = {
     node: true
   },
   extends: [
+    "plugin:prettier/recommended",
     "plugin:vue/essential",
-    "eslint:recommended",
-    "@vue/typescript/recommended",
-    "@vue/prettier",
-    "@vue/prettier/@typescript-eslint"
+    "eslint:recommended"
   ],
   parserOptions: {
-    ecmaVersion: 2020
+    parser: "@typescript-eslint/parser"
   },
   rules: {
-    "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
-    "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off"
+    "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off",
+    "no-unused-vars": "off",
+    "no-undef": "off",
+    "no-empty": "warn",
+    "prettier/prettier": "warn",
+    "no-fallthrough": "off"
   }
 };
